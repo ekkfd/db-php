b61208db6d7f1eafc6c80f4d67b1c540a730bed5
Erik-KK <64156849+Erik-KK@users.noreply.github.com>
2021-08-17 13:57:52 +0300

    v1.0

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-	// database config [config.php] made by ekk
+	// database config [config.php] made by ekk v1.0
 	$dbBaseName = "database name";
 	$dbUserName = "database username";
 	$dbPassword = "database password";
