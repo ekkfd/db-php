7643a0905d674278360da7789bd4385c5c1d665c
Erik-KK <64156849+Erik-KK@users.noreply.github.com>
2021-08-17 13:55:37 +0300

    v1.0

--- db.php
+++ db.php
@@ -1,27 +1,14 @@
 <?php
 // MYSQL database functions [db.php] made by ekk v1.0
 
-	/*
-		* {database} example *
-		{table}
-		`id` | `name` | `password`
-		 1	 |  Ivan  |  pass1
-		 2	 |  Egor  |  pass2
-		 3	 |  Dima  |  pass3
-		 4	 |  Dima  |  pass4
-		 5	 |  Dima  |  pass4
-		 ..  |  ....  |  ....
-	*/ 
-
 	// include  database config
 	include 'modules/config.php';
 
 	// creating database link
 	$dbLink = Null;
 
-	/*
-		creating connection to database
-	*/
+	
+	// -- [creating connection to database] -- //
 	function dbConnect() {
 		global $dbBaseName, $dbUserName, $dbPassword, $dbHostName, $dbLink;
 		$dbLink = mysqli_connect($dbHostName, $dbUserName, $dbPassword, $dbBaseName);
@@ -29,21 +16,16 @@
 		mysqli_query($dbLink, "SET CHARACTER SET 'utf8';");
 		mysqli_query($dbLink, "SET SESSION collation_connection = 'utf8_general_ci';");
 	}
+
 	
-	/*
-		close connection to database
-	*/
+	// -- [close connection to database] -- //
 	function dbDisconnect() {
 		global $dbLink;
 		mysqli_close($dbLink);
 	}
+
 	
-	/*
-		make query
-		+ require $query [SQL code]
-		- return [MYSQL-object]
-		* example: dbQuery('SELECT * FROM `database`');
-	*/
+	// -- [make query] -- //
 	function dbQuery($query) {
 		global $dbLink;
 		dbConnect();
@@ -52,32 +34,8 @@
 		return $ret;
 	}
 	
-	/*
-		get lines by column = value
-
-		+ require:
-				++ $table  [text/table name],
-				++ $column [text/column name where search],
-				++ $value  [text(int)/value wich search in column]
-					+++ $column == '*' and $value == '*' to select all from table
-
-		- return [Array(Array('field' => 'value'))] or `false` if 0 rows returned
-
-		* example:
-				*call* dbGetLines('table', 'name', 'Dima');
-				*return* [
-							[
-								'id'   		=> '3',
-								'name' 		=> 'Dima',
-								'password'  => 'pass3'
-							],
-							[
-								'id'   		=> '4',
-								'name' 		=> 'Dima',
-								'password'  => 'pass4'
-							]
-						 ]
-	*/
+	
+	// -- [lines by column = value] -- //
 	function dbGetLines($table, $column, $value) {
 		if($column == '*' && $value == '*') $q = dbQuery("SELECT * FROM `".$table."`");
 		else $q = dbQuery("SELECT * FROM `".$table."` WHERE `".$column."` = '".$value."'");
@@ -88,30 +46,9 @@
 		} else $ret = false;
 		return $ret;
 	}
-	/*
-		get lines by filter
+	
 
-		+ require:
-			++ $table 	[text/table name],
-			++ $filters [Array('field' => 'value')]
-		
-		- return [Array(Array('field' => 'value'))] or `false` if 0 rows returned
-
-		* example:
-			*call* dbFilter('table', Array('name' => 'Dima', 'password' => 'pass4'));
-			*return* [
-						[
-							'id'   		=> '4',
-							'name' 		=> 'Dima',
-							'password'  => 'pass4'
-						],
-						[
-							'id'   		=> '5',
-							'name' 		=> 'Dima',
-							'password'  => 'pass4'
-						]
-					 ]
-	*/
+	// -- [get lines by filter] -- //
 	function dbFilter($table, $filters) {
 		$q = "SELECT * FROM `".$table.'` WHERE';
 		$i = 1;
@@ -131,23 +68,8 @@
 		return $ret;
 	}
 	
-	/*
-		get 1 line by column = value
-
-		+ require:
-				++ $table  [text/table name],
-				++ $column [text/column name where search],
-				++ $value  [text(int)/value wich search in column]
-		- return [Array('field' => 'value')] or `false` if rows != 1 returned
-
-		* example:
-			*call* dbFilter('table', 'id', 4);
-			*return* [
-						'id'   		=> '4',
-						'name' 		=> 'Dima',
-						'password'  => 'pass4'
-					 ]
-	*/
+	
+	// -- [get 1 line by column = value] -- //
 	function dbGetLine($table, $column, $value) {
 		$q = dbQuery("SELECT * FROM `".$table."` WHERE `".$column."` = '".$value."'");
 		if(mysqli_num_rows($q) == 1) $ret = mysqli_fetch_assoc($q);
@@ -155,21 +77,8 @@
 		return $ret;
 	}
 	
-	/*
-		create line
-
-		+ require:
-				++ $table	[text/table name],
-				++ $columns [Array('column1', 'column2',)],
-				++ $values  [Array('value1', 'value2',)]
-		
-		- return [~nut~]
-
-		* example:
-			*call* dbAddLine('table', ['name', 'password'], ['Artem', 'pass6']);
-			*return* [~nut~]
-
-	*/
+	
+	// -- [create line] -- //
 	function dbAddLine($table, $columns, $values) {
 		$q = "INSERT INTO `".$table."` (";
 		$c = 0;
@@ -189,41 +98,15 @@
 		dbQuery($q);
 	}
 	
-	/*
-		delete line
 
-		+ require:
-				++ $table	[text/table name],
-				++ $column  [text/column name where search],
-				++ $value   [text(int)/value wich search in column]
-		
-		- return [~nut~]
-
-		* example:
-			*call* dbDeleteLine('table', 'id', 6);
-			*return* [~nut~]
-	*/
+	// -- [delete line] -- //
 	function dbDeleteLine($table, $column, $value) {
 		$q = "DELETE FROM `".$table."` WHERE `".$column."` = '".$value."'";
 		dbQuery($q);
 	}
 
-	/*
-		update cells by column = value
-
-		+ require:
-				++ $table	[text/table name],
-				++ $column  [text/column name where search],
-				++ $value   [text(int)/value wich search in column],
-				++ $columns [Array('column1', 'column2',)],
-				++ $values  [Array('value1', 'value2',)]
-		
-		- return [~nut~]
-
-		* example:
-			*call* dbUpdateCells('table', 'id', '5', ['name', 'password'], ['Iva', 'pass123']);
-			*return* [~nut~]
-	*/
+	
+	// -- [update cells by column = value] -- //
 	function dbUpdateCells($table, $column, $value, $columns, $values) {
 		$q = "UPDATE `".$table."` SET ";
 		for($i=0; $i < count($columns); $i++) {
@@ -234,38 +117,18 @@
 		dbQuery($q);
 	}
 	
-	/*
-		get max id in table
-
-		+ require:
-				$table [text/table name]
-		
-		- return [int]
-
-		* example:
-			*call* dbGetMaxID('table');
-			*return* 5
-	*/ 
+	
+	// -- [get max id in table] -- //
 	function dbGetMaxID($table) {
 		return intval(mysqli_fetch_array(dbQuery('SELECT MAX(`id`) FROM `'.$table.'`'))[0]);
 	}
 	
-	/*
-		Jack the ripper :D (split the string and removes emptiness)
-
-		+ require:
-				$data 	  [text/original string],
-				$splitter [text/splitter symb]
-		- return Array('val1', 'val2',)
-
-		* example:
-			*call* dbJack('a,.,b,.,c,.,d', ',.,');
-			*return* ['a', 'b', 'c', 'd']
-	*/
+	
+	// -- [Jack the ripper :D (split the string and removes emptiness)] -- //
 	function dbJack($data, $splitter) {
 		$ret = explode($splitter, $data);
 		for($i = count($ret); $i >= 0 ; $i--) {
-      if($ret[$i] == '') array_splice($ret, $i, 1);
+			if($ret[$i] == '') array_splice($ret, $i, 1);
 		}
 		return $ret;
 	}
